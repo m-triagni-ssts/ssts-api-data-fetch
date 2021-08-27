2dcefa4a8267205a81b63727eb61d632bbcaa539
MariaTriagni <m.triagni.ssts@gmail.com>
2021-08-27 15:15:48 -0700

    Change version

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ module.exports.start = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Go Serverless v2.0! Your function executed successfully!',
         input: event,
       },
       null,
